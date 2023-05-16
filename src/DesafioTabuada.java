6ae2e2f1038e549b978c1a052200a75faf73c5eb
pedro <pedro.paz13@hotmail.com>
2023-05-16 11:25:44 -0300

    subindo mais uma classe com desafio de projeto

---
+++ src/DesafioTabuada.java
@@ -0,0 +1,15 @@
+
+public class DesafioTabuada {
+
+	public static void main(String[] args) {
+
+		/**
+		 * desenvolva uma tabuada que possa devolver para o usuario qualquer tabuada de multiplicação
+		 * entre 1 e 10
+		 */
+		
+		
+		
+	}
+
+}
